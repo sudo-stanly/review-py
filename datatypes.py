62723ab4e6a4fe83ec837ecb9265b458576d1214
stanly-sudo <alys.git24@gmail.com>
2025-07-10 10:21:52 +0800

    data types

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+#Data types:
+
+#string
+var = str('Hello world')
+
+#Numeric types
+var = int(1234)
+var = float(12/2)
+var = complex(334j)
+
+#sequence types
+list = [1, 2, 3]; #print(list)
+tuple = (1, 2, 3); #print(tuple)
+rg = range(6); #print(rg)
+
+
+#mapping
+dictionary = {"name": str("John Doe"), "age":int(22)}; #print(dictionary["name"])
+#for x in dictionary.values(): #values
+    #print(x) 
+#for key, value in dictionary.items(): #key and values
+    #print(key, value)
+    
+#Boolean
+isStudent= True; isGraduated = False; print(f"Student:\t{isStudent}\nGraduate:\t{isGraduated}")
+
+
